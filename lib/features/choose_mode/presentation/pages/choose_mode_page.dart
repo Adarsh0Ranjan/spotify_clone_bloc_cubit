6f9cf8a3d0641ee90a4cec771b8078bf31321613
Adarsh Ranjan <adarsh.ranjan@roro.io>
2025-07-09 11:41:18 +0530

    choose mode logic added

--- lib/features/choose_mode/presentation/pages/choose_mode_page.dart
+++ lib/features/choose_mode/presentation/pages/choose_mode_page.dart
@@ -1,8 +1,10 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:spotify_clone_flutter/core/config/theme/app_colors.dart';
+import 'package:spotify_clone_flutter/features/choose_mode/presentation/bloc/theme_cubit.dart';
 
 import '../../../../common/widgets/button/basic_app_button.dart';
 import '../../../../core/config/assets/app_image.dart';
@@ -50,25 +52,32 @@ class ChooseModePage extends StatelessWidget {
                       children: [
                         Column(
                           children: [
-                            ClipOval(
-                              child: BackdropFilter(
-                                filter: ImageFilter.blur(
-                                  sigmaX: 10,
-                                  sigmaY: 10,
-                                ),
-                                child: Container(
-                                  height: 80,
-                                  width: 80,
-                                  decoration: BoxDecoration(
-                                    shape: BoxShape.circle,
-                                    color: const Color(
-                                      0xff30393C,
-                                    ).withAlpha(128),
-                                    // image: DecorationImage(image: SvgPicture.asset(AppVectors.spotifyLogo))
+                            GestureDetector(
+                              onTap: () {
+                                context.read<ThemeCubit>().updateTheme(
+                                  ThemeMode.dark,
+                                );
+                              },
+                              child: ClipOval(
+                                child: BackdropFilter(
+                                  filter: ImageFilter.blur(
+                                    sigmaX: 10,
+                                    sigmaY: 10,
                                   ),
-                                  child: SvgPicture.asset(
-                                    AppVectors.moon,
-                                    fit: BoxFit.none,
+                                  child: Container(
+                                    height: 80,
+                                    width: 80,
+                                    decoration: BoxDecoration(
+                                      shape: BoxShape.circle,
+                                      color: const Color(
+                                        0xff30393C,
+                                      ).withAlpha(128),
+                                      // image: DecorationImage(image: SvgPicture.asset(AppVectors.spotifyLogo))
+                                    ),
+                                    child: SvgPicture.asset(
+                                      AppVectors.moon,
+                                      fit: BoxFit.none,
+                                    ),
                                   ),
                                 ),
                               ),
@@ -89,25 +98,32 @@ class ChooseModePage extends StatelessWidget {
 
                         Column(
                           children: [
-                            ClipOval(
-                              child: BackdropFilter(
-                                filter: ImageFilter.blur(
-                                  sigmaX: 10,
-                                  sigmaY: 10,
-                                ),
-                                child: Container(
-                                  height: 80,
-                                  width: 80,
-                                  decoration: BoxDecoration(
-                                    shape: BoxShape.circle,
-                                    color: const Color(
-                                      0xff30393C,
-                                    ).withAlpha(128),
-                                    // image: DecorationImage(image: SvgPicture.asset(AppVectors.spotifyLogo))
+                            GestureDetector(
+                              onTap: () {
+                                context.read<ThemeCubit>().updateTheme(
+                                  ThemeMode.light,
+                                );
+                              },
+                              child: ClipOval(
+                                child: BackdropFilter(
+                                  filter: ImageFilter.blur(
+                                    sigmaX: 10,
+                                    sigmaY: 10,
                                   ),
-                                  child: SvgPicture.asset(
-                                    AppVectors.sun,
-                                    fit: BoxFit.none,
+                                  child: Container(
+                                    height: 80,
+                                    width: 80,
+                                    decoration: BoxDecoration(
+                                      shape: BoxShape.circle,
+                                      color: const Color(
+                                        0xff30393C,
+                                      ).withAlpha(128),
+                                      // image: DecorationImage(image: SvgPicture.asset(AppVectors.spotifyLogo))
+                                    ),
+                                    child: SvgPicture.asset(
+                                      AppVectors.sun,
+                                      fit: BoxFit.none,
+                                    ),
                                   ),
                                 ),
                               ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,46 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:spotify_clone_flutter/core/config/theme/app_theme.dart';
+import 'package:spotify_clone_flutter/features/choose_mode/presentation/bloc/theme_cubit.dart';
 
 import 'features/splash/presentation/pages/splash.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  HydratedBloc.storage = await HydratedStorage.build(
+    storageDirectory:
+        kIsWeb
+            ? HydratedStorageDirectory.web
+            : HydratedStorageDirectory((await getTemporaryDirectory()).path),
+  );
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: AppTheme.lightTheme,
-      debugShowCheckedModeBanner: false,
-      home: const SplashPage(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<ThemeCubit>(create: (context) => ThemeCubit()),
+        // Add more BlocProviders here if needed
+      ],
+      child: BlocBuilder<ThemeCubit, ThemeMode>(
+        builder: (context, mode) {
+          return MaterialApp(
+            title: 'Flutter Demo',
+            theme: AppTheme.lightTheme,
+            darkTheme: AppTheme.darkTheme,
+            themeMode: mode,
+            debugShowCheckedModeBanner: false,
+            home: const SplashPage(),
+          );
+        },
+      ),
     );
   }
 }
